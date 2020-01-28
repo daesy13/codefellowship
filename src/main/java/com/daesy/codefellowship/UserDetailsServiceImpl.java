d1871cbeee1f81d48f7af658325001c02b86ef88
daesy <daesyramos@hotmail.com>
2020-01-28 14:06:32 -0800

    more config

---
+++ src/main/java/com/daesy/codefellowship/UserDetailsServiceImpl.java
@@ -0,0 +1,9 @@
+package com.daesy.codefellowship;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+//@Service
+//public class UserDetailsServiceImpl implements UserDetailsService {
+//
+//}
